09ead7a206d69c88160b4ec762477e3e6fa35fec
yeonggyu_lim <yeonggyu_lim@tmax.co.kr>
2019-12-22 15:44:50 +0900

    #2.0 Make News Viewer UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList'
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=3f62e272920947c3b4fee20cb22cee7a',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>  
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
+  return <NewsList />
 };
 
 export default App;
