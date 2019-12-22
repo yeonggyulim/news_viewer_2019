5e129bc3ee46aee7d69336ba263808b8637592a5
yeonggyu_lim <yeonggyu_lim@tmax.co.kr>
2019-12-22 16:04:55 +0900

    #3.0 Get Data from News API

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -15,23 +16,42 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const sampleArticle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160',
-};
-
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // async를 사용하는 함수 따로 선언
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=3f62e272920947c3b4fee20cb22cee7a');
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    // 대기 중일 때
+    if (loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>;
+    }
+    // 아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
+        return null;
+    }
+
+    // articles 값이 유효할 때
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
+    
     );
 };
 
