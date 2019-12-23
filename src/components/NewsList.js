b652b6185e75ee1237e42f43a6d29f9c96382599
yeonggyu_lim <yeonggyu_lim@tmax.co.kr>
2019-12-23 14:05:44 +0900

    #5.0 Create usePromise Custom Hook

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -17,35 +18,29 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({category}) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        // async를 사용하는 함수 따로 선언
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '': `&category=${category}`;
-                const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=3f62e272920947c3b4fee20cb22cee7a`);
-                setArticles(response.data.articles);
-            } catch (e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=3f62e272920947c3b4fee20cb22cee7a`,
+        );
     }, [category]);
 
     // 대기 중일 때
     if (loading) {
         return <NewsListBlock>대기 중...</NewsListBlock>;
     }
-    // 아직 articles 값이 설정되지 않았을 때
-    if (!articles) {
+    // 아직 response 값이 설정되지 않았을 때
+    if (!response) {
         return null;
     }
 
-    // articles 값이 유효할 때
+    // 에러가 발생했을 때
+    if (error) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>;
+    }
+
+    // response 값이 유효할 때
+    const { articles } = response.data;
     return (
         <NewsListBlock>
             {articles.map(article => (
